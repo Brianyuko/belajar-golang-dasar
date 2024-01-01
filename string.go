8332de71b2e959c5fbbef7abd544d3ba669dde46
Brianyuko <brianyuko45@gmail.com>
2024-01-01 20:16:45 +0700

    string

---
+++ string.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Brian")
+	fmt.Println(len("Brian"))          //Count Character in String => 5
+	fmt.Println("Brian Yuko"[0])       // Return Type Byte
+	fmt.Println("Brian Yuko Putra"[1]) // Return Type Byte
+}
