0294416201c13e604e0f60b75510f895cfd3ce9a
Brianyuko <brianyuko45@gmail.com>
2024-01-01 20:35:29 +0700

    constant

---
+++ constant.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	const firstName = "Brian"
+	const lastName = "Yuko"
+	// Error
+	// firstName = "Putra"
+
+	const (
+		city    = "Jakarta"
+		country = "Indonesia"
+	)
+}
